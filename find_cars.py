f83e424b9678b35169ba503b8d9408f30cd992f5
jonathancmitchell <jmitchell1991@gmail.com>
2017-05-02 18:53:10 -0700

    [feature] - creates Car_Finder class with process_image function to find bounding boxes and process image with heatmaps

--- find_cars.py
+++ find_cars.py
@@ -5,16 +5,17 @@ import pickle
 import cv2
 import glob
 import settings
-
+from scipy.ndimage.measurements import label
 from helpers import convert_color, \
     get_hog_features, \
     bin_spatial, \
     color_hist, \
-    draw_boxes
+    draw_labeled_boxes, \
+    add_heat, \
+    apply_threshold
 
 
-
-img = mpimg.imread('./test_images/test1.jpg')
+img = mpimg.imread('./test_images/test4.jpg')
 
 
 
@@ -28,118 +29,141 @@ cell_per_block = pickle_data['cell_per_block']
 spatial_size = pickle_data['spatial_size']
 hist_bins = pickle_data['hist_bins']
 
-#
-#
-# Define a single function that can extract features using hog sub-sampling and make predictions
-def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins):
-    """
-    After training our SVM, we utilize find_cars in conjunction with our SVM predictor to locate the cars
-
-    :param img: Image passed in as RGB as shape (720, 1280, 3)
-    :param ystart: Starting point for search in y
-    :param ystop: Stopping point for search in y
-    :param scale: Scale to shrink or expand the search region
-    :param svc: Linear Support Vector Machine Classifier which was trained on training data in model.py
-    :param X_scaler: Linear SVC Scaler which was fit on the training data in model.py
-    :param orient: # of orientations, specified as an integer, represents the number of orientation bins that the 
-    gradient information will be split up into the histogram. Typically ~6 to 12 .
-    :param pix_per_cell: # of pixels per cell passed to HOG as (pix_per_cell, pix_per_cell) tuple. 
-    They are commonly chosen to be square
-    :param cell_per_block: number of cells per cell_block passed to HOG as (cell_per_block, cell_per_block) tuple. 
-    It specifies the local area over which the histogram counts in a given cell will be normalized.
-    :param spatial_size: tuple size passed into bin_spatial in helper.py, used to resize the image and determines
-     the length of the features return from bin_spatial after ravel()-ing
-    :param hist_bins: number of bins inside np.histogram of a specific channel inside the image, correlates to the resolution of the histogram features, 
-    each bin represents a value that has a corresponding feature, more bins = more features captured
-    :return: draw_img (720, 1280, 3) RGB copy of img that has boxes drawn on it by using cv2.rectangle
-    """
-    draw_img = np.copy(img)
-    # img = img.astype(np.float32) / 255
-
-    img_tosearch = img[ystart:ystop, :, :]
-    ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YCrCb')
-    if scale != 1:
-        imshape = ctrans_tosearch.shape
-        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1] / scale), np.int(imshape[0] / scale)))
-
-    # Channel extraction
-    ch1 = ctrans_tosearch[:, :, 0]
-    ch2 = ctrans_tosearch[:, :, 1]
-    ch3 = ctrans_tosearch[:, :, 2]
-
-    # Define blocks and steps as above
-    nxblocks = (ch1.shape[1] // pix_per_cell) - cell_per_block + 1
-    nyblocks = (ch1.shape[0] // pix_per_cell) - cell_per_block + 1
-    nfeat_per_block = orient * cell_per_block ** 2
-
-    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
-    window = 64
-    nblocks_per_window = (window // pix_per_cell) - cell_per_block + 1
-    cells_per_step = 2  # Instead of overlap, define how many cells to step
-    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
-    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
-
-    # Compute individual channel HOG features for the entire image
-    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
-    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
-    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
-
-    bboxes = []
-
-    for xb in range(nxsteps):
-        for yb in range(nysteps):
-            ypos = yb * cells_per_step
-            xpos = xb * cells_per_step
-            # Extract HOG for this patch
-
-            # Flatten the HOG features for each channel position
-            hog_feat1 = hog1[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
-            hog_feat2 = hog2[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
-            hog_feat3 = hog3[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
-            # Build hog_features array by stacking each channel
-            hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
-
-            xleft = xpos * pix_per_cell
-            ytop = ypos * pix_per_cell
-
-            # Extract the image patch
-            subimg = cv2.resize(ctrans_tosearch[ytop:ytop + window, xleft:xleft + window], (64, 64))
-
-            # Get color features
-            spatial_features = bin_spatial(subimg, size=spatial_size)
-            hist_features = color_hist(subimg, nbins=hist_bins)
-
-            # Scale features and make a prediction
-            test_features = X_scaler.transform(
-                np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
-            # test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))
-            test_prediction = svc.predict(test_features)
-
-            if test_prediction == 1:
-                xbox_left = np.int(xleft * scale)
-                ytop_draw = np.int(ytop * scale)
-                win_draw = np.int(window * scale)
-                bboxes.append(((xbox_left, ytop_draw + ystart), (xbox_left + win_draw, ytop_draw + win_draw + ystart)))
-    draw_img = draw_boxes(draw_img, bboxes)
-    return draw_img
 
 
-ystart = 400
-ystop = 656
-scale = 1.5
+class Car_Finder():
+    def __init__(self, ystart = 400, ystop = 656, scale = 1.5):
+        self.ystart = ystart
+        self.ystop = ystop
+        self.scale = scale
+        self.threshold = 2
+        self.heatmaps = []
+        self.heat = np.zeros((settings.IMG_HEIGHT, settings.IMG_WIDTH), dtype = np.float32) # maybe chance dtype
 
-out_img = find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size,
-                    hist_bins)
+    def process_image(self, img):
+        bboxes = self.find_cars(img, self.ystart, self.ystop, self.scale, svc, X_scaler, orient, pix_per_cell, cell_per_block,
+                  spatial_size,
+                  hist_bins)
 
-# HOG Subsample search region
-# y_vals = np.linspace(ystart, ystart, out_img.shape[1] - 1 )
-# x_vals = np.linspace(0, out_img.shape[1], out_img.shape[1] - 1)
-# y_vals2 = np.linspace(ystop, ystop, out_img.shape[1] - 1)
-# plt.plot(x_vals, y_vals, 's-')
-# plt.plot(x_vals, y_vals2, 'bs-')
-# plt.title('HOG Subsample search region')
+        self.build_and_update_heatmap(bboxes)
+        heat = self.apply_threshold(self.threshold)
+        heatmap = np.clip(heat, 0, 255)
+        labels = label(heatmap)
+        draw_img = draw_labeled_boxes(np.copy(img), labels)
+        plt.imshow(labels[0], cmap='gray')
+        plt.show()
+        return draw_img
+
+    def build_and_update_heatmap(self, bboxes):
+        self.heat = add_heat(self.heat, bboxes)
+        self.heatmaps.append(self.heat)
+
+    def apply_threshold(self, threshold):
+        if len(self.heatmaps) > 7:
+            # TODO: Set heatmap equal to average of last 7 heatmaps
+            print('there are at least 7 heatmaps')
+        else:
+            heatmap = self.heat
+        return apply_threshold(heatmap, threshold)
+
+    def find_cars(self, img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size,
+                  hist_bins):
+        """
+        After training our SVM, we utilize find_cars in conjunction with our SVM predictor to locate the cars
+
+        :param img: Image passed in as RGB as shape (720, 1280, 3)
+        :param ystart: Starting point for search in y
+        :param ystop: Stopping point for search in y
+        :param scale: Scale to shrink or expand the search region
+        :param svc: Linear Support Vector Machine Classifier which was trained on training data in model.py
+        :param X_scaler: Linear SVC Scaler which was fit on the training data in model.py
+        :param orient: # of orientations, specified as an integer, represents the number of orientation bins that the 
+        gradient information will be split up into the histogram. Typically ~6 to 12 .
+        :param pix_per_cell: # of pixels per cell passed to HOG as (pix_per_cell, pix_per_cell) tuple. 
+        They are commonly chosen to be square
+        :param cell_per_block: number of cells per cell_block passed to HOG as (cell_per_block, cell_per_block) tuple. 
+        It specifies the local area over which the histogram counts in a given cell will be normalized.
+        :param spatial_size: tuple size passed into bin_spatial in helper.py, used to resize the image and determines
+         the length of the features return from bin_spatial after ravel()-ing
+        :param hist_bins: number of bins inside np.histogram of a specific channel inside the image, correlates to the resolution of the histogram features, 
+        each bin represents a value that has a corresponding feature, more bins = more features captured
+        :return: draw_img (720, 1280, 3) RGB copy of img that has boxes drawn on it by using cv2.rectangle
+        """
+        draw_img = np.copy(img)
+        # img = img.astype(np.float32) / 255
+
+        img_tosearch = img[ystart:ystop, :, :]
+        ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YCrCb')
+        if scale != 1:
+            imshape = ctrans_tosearch.shape
+            ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1] / scale), np.int(imshape[0] / scale)))
+
+        # Channel extraction
+        ch1 = ctrans_tosearch[:, :, 0]
+        ch2 = ctrans_tosearch[:, :, 1]
+        ch3 = ctrans_tosearch[:, :, 2]
+
+        # Define blocks and steps as above
+        nxblocks = (ch1.shape[1] // pix_per_cell) - cell_per_block + 1
+        nyblocks = (ch1.shape[0] // pix_per_cell) - cell_per_block + 1
+        nfeat_per_block = orient * cell_per_block ** 2
+
+        # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+        window = 64
+        nblocks_per_window = (window // pix_per_cell) - cell_per_block + 1
+        cells_per_step = 2  # Instead of overlap, define how many cells to step
+        nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
+        nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+
+        # Compute individual channel HOG features for the entire image
+        hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+        hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+        hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+
+        bboxes = []
+
+        for xb in range(nxsteps):
+            for yb in range(nysteps):
+                ypos = yb * cells_per_step
+                xpos = xb * cells_per_step
+                # Extract HOG for this patch
+
+                # Flatten the HOG features for each channel position
+                hog_feat1 = hog1[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+                hog_feat2 = hog2[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+                hog_feat3 = hog3[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+                # Build hog_features array by stacking each channel
+                hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
+
+                xleft = xpos * pix_per_cell
+                ytop = ypos * pix_per_cell
+
+                # Extract the image patch
+                subimg = cv2.resize(ctrans_tosearch[ytop:ytop + window, xleft:xleft + window], (64, 64))
+
+                # Get color features
+                spatial_features = bin_spatial(subimg, size=spatial_size)
+                hist_features = color_hist(subimg, nbins=hist_bins)
+
+                # Scale features and make a prediction
+                test_features = X_scaler.transform(
+                    np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
+                # test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))
+                test_prediction = svc.predict(test_features)
+
+                if test_prediction == 1:
+                    xbox_left = np.int(xleft * scale)
+                    ytop_draw = np.int(ytop * scale)
+                    win_draw = np.int(window * scale)
+                    bboxes.append(
+                        ((xbox_left, ytop_draw + ystart), (xbox_left + win_draw, ytop_draw + win_draw + ystart)))
+        return bboxes
 
 
-plt.imshow(out_img)
-plt.title('HOG subsampling method on test1')
-plt.show()
+cf = Car_Finder()
+out_img = cf.process_image(img)
+
+
+
+# plt.imshow(out_img)
+# plt.show()
